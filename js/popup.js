30018ba8c8356667dbce88b2df9b673500e733b8
Fabio <fabioeidishi@hotmail.com>
2022-02-23 15:35:32 -0300

    primeiro commit

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+
+function popup(){
+  let modal = document.querySelector('.modal');
+
+  modal.style.display = 'block';
+}
+
+function fechar() {
+  let modal = document.querySelector('.modal');
+  
+  modal.style.display = 'none';
+}
